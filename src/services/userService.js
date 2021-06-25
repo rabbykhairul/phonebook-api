eb66ca7c01365d75212f866447d1997f088b1e9a
rabbyKhairul <rabbykhairul@gmail.com>
2021-06-25 15:26:29 +0600

    user registration req - check if email in is already used or not before creating new user

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,10 +2,28 @@ const User = require("../models/user");
 
 const saveNewUser = async (userDetails) => {
   try {
-    const newUser = new User(userDetails);
-    await newUser.save();
+    const IsEmailAlreadyInUse = await doesUserExistsByEmail(userDetails.email);
+    if (IsEmailAlreadyInUse) {
+      console.log("\n----");
+      console.log(
+        `Aborting new user save operation as an user exists with email: ${userDetails.email}`
+      );
+      console.log("----\n");
 
-    return true;
+      return {
+        message:
+          "Aborting new user save operation as the email is already in use.",
+        savedSuccessfully: false,
+      };
+    } else {
+      const newUser = new User(userDetails);
+      await newUser.save();
+
+      return {
+        message: "New user saved successfully",
+        savedSuccessfully: true,
+      };
+    }
   } catch (err) {
     console.log("\n===");
     console.log("error while saving new user: ", err);
@@ -13,6 +31,12 @@ const saveNewUser = async (userDetails) => {
 
     return false;
   }
+};
+
+const doesUserExistsByEmail = async (email) => {
+  const user = await User.findOne({ email });
+  if (user) return true;
+  else false;
 };
 
 module.exports = { saveNewUser };
