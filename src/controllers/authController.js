d68933f78e0b7e8c13afe7b9acd7e7eaca2fb90a
rabbyKhairul <rabbykhairul@gmail.com>
2021-06-25 17:53:26 +0600

    on login request, check email & compare password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const userService = require("../services/userService");
+const helper = require("../utils/helper");
 
 const hanldeUserRegistration = async (req, res) => {
   try {
@@ -20,6 +21,36 @@ const hanldeUserRegistration = async (req, res) => {
   }
 };
 
+const handleUserLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userService.getUserByEmail(email);
+    if (user) {
+      const isCorrectPassword = await helper.checkPassword(
+        user.password,
+        password
+      );
+      if (isCorrectPassword)
+        res.status(200).json({ message: "password matched...", status: 200 });
+      else
+        res
+          .status(400)
+          .json({ message: "password is incorrect...", status: 400 });
+    } else
+      res
+        .status(400)
+        .json({ message: "Email is not registered...", status: 400 });
+  } catch (err) {
+    console.log("\n---");
+    console.log("Error on user login...");
+    console.log("error: ", err);
+    console.log("---\n");
+
+    res.status(500).json({ message: "Something went wrong...", status: 500 });
+  }
+};
+
 module.exports = {
   hanldeUserRegistration,
+  handleUserLogin,
 };
