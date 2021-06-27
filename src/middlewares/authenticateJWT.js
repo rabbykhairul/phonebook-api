c146040422a9b01220b507c8a66b5726c0178396
rabbyKhairul <rabbykhairul@gmail.com>
2021-06-27 14:04:20 +0600

    authenticateJWT middleware - add helpfull console log messages

--- src/middlewares/authenticateJWT.js
+++ src/middlewares/authenticateJWT.js
@@ -2,11 +2,16 @@ const authenticateJWT = (req, res, next) => {
   const bearerToken = req.get("authorization")?.split(" ")[1];
 
   if (bearerToken) {
-  } else
+  } else {
+    console.log("\n---");
+    console.log(`Denying access to to route path: ${req.path}`);
+    console.log("Reason: No bearer token in the req header...");
+    console.log("---\n");
     res.status(400).json({
       message: "Access denied. Try logging in or create an account.",
       status: 400,
     });
+  }
 };
 
 module.exports = authenticateJWT;
