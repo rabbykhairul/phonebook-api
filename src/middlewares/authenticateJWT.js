de5c00f371205ba7157b155286b05f6671ad13e7
rabbyKhairul <rabbykhairul@gmail.com>
2021-06-27 15:00:53 +0600

    authenticateJWT middleware - after successful token docode, give control to next()

--- src/middlewares/authenticateJWT.js
+++ src/middlewares/authenticateJWT.js
@@ -1,11 +1,15 @@
+const helper = require("../utils/helper");
+
 const authenticateJWT = (req, res, next) => {
   const bearerToken = req.get("authorization")?.split(" ")[1];
+  const decodedToken = helper.validateToken(bearerToken);
 
-  if (bearerToken) {
+  if (decodedToken) {
+    next();
   } else {
     console.log("\n---");
     console.log(`Denying access to to route path: ${req.path}`);
-    console.log("Reason: No bearer token in the req header...");
+    console.log(`Reason: ${bearerToken ? "Invalid" : "No"} bearer token in the req header....`);
     console.log("---\n");
     res.status(400).json({
       message: "Access denied. Try logging in or create an account.",
